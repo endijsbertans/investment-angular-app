111df425a33272dd2fc5412203bb4dd115bd4d60
endijsbertans <endijsbertans@gmail.com>
2024-09-12 14:08:13 +0300

    Changed app to use signals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
 import {HeaderComponent} from "./header/header.component";
 import {UserInputComponent} from "./user-input/user-input.component";
 import {InvestmentInput} from "./investment-input-model";
@@ -16,7 +16,8 @@ import {investmentResult} from "./investment-results-model";
   ]
 })
 export class AppComponent {
-  resultsData?: investmentResult[];
+  resultsData = signal<investmentResult[] | undefined>(undefined);
+
   onCalculateInvestmentResults(data: InvestmentInput) {
     const {initialInvestment, annualInvestment, expectedReturn, duration} = data;
     const annualData = [];
@@ -37,6 +38,6 @@ export class AppComponent {
         totalAmountInvested: initialInvestment + annualInvestment * year,
       });
     }
-    this.resultsData = annualData;
+    this.resultsData.set(annualData);
   }
 }
--- src/app/investment-results/investment-results.component.ts
+++ src/app/investment-results/investment-results.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, input, Input} from '@angular/core';
 import {investmentResult} from "../investment-results-model";
 import {CurrencyPipe} from "@angular/common";
 
@@ -12,6 +12,6 @@ import {CurrencyPipe} from "@angular/common";
   styleUrl: './investment-results.component.css'
 })
 export class InvestmentResultsComponent {
-  @Input() results?: investmentResult[];
+  results=  input<investmentResult[]>();
 
 }
