fc0e995ae7c5659b22e267c231f4ae8309a4ec99
endijsbertans <endijsbertans@gmail.com>
2024-09-12 14:53:49 +0300

    Changed app to use signals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
-import {Component, signal} from '@angular/core';
+import {Component} from '@angular/core';
 import {HeaderComponent} from "./header/header.component";
 import {UserInputComponent} from "./user-input/user-input.component";
-import {InvestmentInput} from "./investment-input-model";
+
 import {InvestmentResultsComponent} from "./investment-results/investment-results.component";
-import {investmentResult} from "./investment-results-model";
+
 
 @Component({
   selector: 'app-root',
@@ -16,28 +16,4 @@ import {investmentResult} from "./investment-results-model";
   ]
 })
 export class AppComponent {
-  resultsData = signal<investmentResult[] | undefined>(undefined);
-
-  onCalculateInvestmentResults(data: InvestmentInput) {
-    const {initialInvestment, annualInvestment, expectedReturn, duration} = data;
-    const annualData = [];
-    let investmentValue = initialInvestment;
-
-    for (let i = 0; i < duration; i++) {
-      const year = i + 1;
-      const interestEarnedInYear = investmentValue * (expectedReturn / 100);
-      investmentValue += interestEarnedInYear + annualInvestment;
-      const totalInterest =
-        investmentValue - annualInvestment * year - initialInvestment;
-      annualData.push({
-        year: year,
-        interest: interestEarnedInYear,
-        valueEndOfYear: investmentValue,
-        annualInvestment: annualInvestment,
-        totalInterest: totalInterest,
-        totalAmountInvested: initialInvestment + annualInvestment * year,
-      });
-    }
-    this.resultsData.set(annualData);
-  }
 }
--- src/app/investment-results/investment-results.component.ts
+++ src/app/investment-results/investment-results.component.ts
@@ -1,6 +1,6 @@
-import {Component, input, Input} from '@angular/core';
-import {investmentResult} from "../investment-results-model";
+import {Component, inject,} from '@angular/core';
 import {CurrencyPipe} from "@angular/common";
+import {InvestmentService} from "../investment.service";
 
 @Component({
   selector: 'app-investment-results',
@@ -12,6 +12,9 @@ import {CurrencyPipe} from "@angular/common";
   styleUrl: './investment-results.component.css'
 })
 export class InvestmentResultsComponent {
-  results=  input<investmentResult[]>();
+  private investmentService = inject(InvestmentService);
 
+  get results(){
+    return this.investmentService.resultsData;
+  }
 }
