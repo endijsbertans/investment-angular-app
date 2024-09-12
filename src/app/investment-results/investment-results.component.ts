605a7f11956abeaa934362dd86b98c376a935e5d
endijsbertans <endijsbertans@gmail.com>
2024-09-12 13:54:05 +0300

    first commit

---
+++ src/app/investment-results/investment-results.component.ts
@@ -0,0 +1,17 @@
+import {Component, Input} from '@angular/core';
+import {investmentResult} from "../investment-results-model";
+import {CurrencyPipe} from "@angular/common";
+
+@Component({
+  selector: 'app-investment-results',
+  standalone: true,
+  imports: [
+    CurrencyPipe
+  ],
+  templateUrl: './investment-results.component.html',
+  styleUrl: './investment-results.component.css'
+})
+export class InvestmentResultsComponent {
+  @Input() results?: investmentResult[];
+
+}
